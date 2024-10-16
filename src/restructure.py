ba5612a1a5c30dd5c54fe5807dbafa5f54506caa
cmclark00 <cmclark00@gmail.com>
2024-10-16 00:53:41 -0400

    working on os list

--- src/restructure.py
+++ src/restructure.py
@@ -1,43 +1,62 @@
 import json
-from collections import defaultdict
 
-# Load the JSON file
+# Load the original JSON
 with open('os_list.json', 'r') as f:
     data = json.load(f)
 
-# Initialize a new dictionary to store the restructured data
+# Create a dictionary to map device tags to manufacturers
+device_to_manufacturer = {}
+
+# Populate the device_to_manufacturer map from the "imager" section
+for manufacturer in data["imager"]["manufacturers"]:
+    manufacturer_name = manufacturer["name"]
+    for device in manufacturer["devices"]:
+        for tag in device["tags"]:
+            device_to_manufacturer[tag] = manufacturer_name
+
+# Initialize the new structure
 restructured_data = {
+    "os_list": [],
     "imager": {
-        "manufacturers": []
+        "latest_version": "1.6",
+        "url": "https://www.raspberrypi.org/software/",
+        "default_os": "muOS 2410.1 Banana for RG35XX Plus",
+        "embedded_default_os": "muOS 2410.1 Banana for RG35XX Plus",
+        "embedded_default_destination": "/dev/mmcblk0p1"
     }
 }
 
-# Create a dictionary to hold manufacturers temporarily
-manufacturers = defaultdict(list)
-
-# Go through the devices listed in the original file
-for device_entry in data['imager']['devices']:
-    # Extract manufacturer name from the device name
-    manufacturer_name = device_entry['name'].split()[0]
-    # Remove manufacturer prefix from the device name to keep it clean
-    device_name = " ".join(device_entry['name'].split()[1:])
+# Go through the OS list and reformat the data
+for os_entry in data["os_list"]:
+    # Create a new entry for each OS in the required format
+    new_os_entry = {
+        "name": os_entry["name"],
+        "description": os_entry["description"],
+        "icon": os_entry["icon"],
+        "url": os_entry["url"],
+        "release_date": os_entry["release_date"],
+        "image_download_size": os_entry["image_download_size"],
+        "extract_size": os_entry["extract_size"],
+        "extract_sha256": os_entry["extract_sha256"],
+        "devices": []
+    }
     
-    # Add device entry to the manufacturer's list
-    manufacturers[manufacturer_name].append({
-        "name": device_name,
-        "tags": device_entry['tags'],
-        "icon": device_entry['icon']
-    })
+    # Add compatible device info under "devices" key
+    for device in os_entry.get("devices", []):
+        manufacturer = device_to_manufacturer.get(device, "Unknown")
+        new_device_entry = {
+            "name": device,
+            "manufacturer": manufacturer,
+            "tags": [device],
+            "icon": f"https://github.com/cmclark00/retro-imager/blob/dev/src/icons/{device}.png?raw=true"
+        }
+        new_os_entry["devices"].append(new_device_entry)
 
-# Transform the manufacturers dictionary into the desired JSON structure
-for manufacturer, devices in manufacturers.items():
-    restructured_data['imager']['manufacturers'].append({
-        "name": manufacturer,
-        "devices": devices
-    })
+    # Add the new OS entry to the restructured data
+    restructured_data["os_list"].append(new_os_entry)
 
-# Save the new JSON structure to a new file
+# Save the restructured JSON to a new file
 with open('restructured_os_list.json', 'w') as f:
     json.dump(restructured_data, f, indent=4)
 
-print("JSON restructuring completed. Check 'restructured_os_list.json' for the updated file.")
+print("Restructuring completed. Check 'restructured_os_list.json' for the updated file.")
